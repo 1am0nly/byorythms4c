35737e8c410397814a6051c58b713abdabedf114
Anton Ignasev <ant.ignasev@gmail.com>
2026-07-10 15:14:20 +0300

    fix: замена FlutterActivity на FlutterFragmentActivity для совместимости с local_auth

--- android/app/src/main/kotlin/com/ignasev/biorhythms_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/ignasev/biorhythms_flutter/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.ignasev.biorhythms_flutter
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
